1cd5bb2bbe38f1b783656cd1ae07370cb6ddee45
akahige <akahigeg@gmail.com>
2013-10-12 13:13:00 +0900

    change README format

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,6 @@ maintainer       "akahigeg"
 maintainer_email "akahigeg@gmail.com"
 license          "MIT"
 description      "install rbenv and global ruby"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1"
 depends          "rbenv"
