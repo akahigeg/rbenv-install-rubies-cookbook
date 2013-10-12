0d144c719f87adae478932fcc270c8035030f0b7
akahige <akahigeg@gmail.com>
2013-10-12 13:49:59 +0900

    add multiple ruby versions support

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
-node.default[:site_rbenv][:global_ruby_version] = '2.0.0-p247'
-node.default[:site_rbenv][:global_ruby_gems] = ['bundler', 'rbenv-rehash', 'pry']
-node.default[:site_rbenv][:lib_packages] = []
+node.default[:rbenv_install_rubies][:global_version] = '2.0.0-p247'
+node.default[:rbenv_install_rubies][:gems] = ['bundler', 'rbenv-rehash', 'pry']
+node.default[:rbenv_install_rubies][:other_versions] = []
+node.default[:rbenv_install_rubies][:lib_packages] = []
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,20 +7,32 @@ include_recipe "rbenv::default"
 include_recipe "rbenv::ruby_build"
 
 # install library packages that is required from ruby or gems
-node[:site_rbenv][:lib_packages].each do |pkg|
+node[:rbenv_install_rubies][:lib_packages].each do |pkg|
   package pkg
 end
 
 # rbenv
-GLOBAL_RUBY_VERSION = node[:site_rbenv][:global_ruby_version]
+GLOBAL_RUBY_VERSION = node[:rbenv_install_rubies][:global_version]
+OTHER_RUBY_VERSIONS = node[:rbenv_install_rubies][:other_versions]
+
 rbenv_ruby GLOBAL_RUBY_VERSION do
   ruby_version GLOBAL_RUBY_VERSION
   global true
 end
 
-node[:site_rbenv][:global_ruby_gems].each do |g|
-  rbenv_gem g do
-    ruby_version GLOBAL_RUBY_VERSION
+OTHER_RUBY_VERSIONS.each do |v|
+  rbenv_ruby v do
+    ruby_version v
+  end
+end
+
+all_versions = OTHER_RUBY_VERSIONS.push(GLOBAL_RUBY_VERSION)
+
+all_versions.each do |v|
+  node[:rbenv_install_rubies][:gems].each do |g|
+    rbenv_gem g do
+      ruby_version v
+    end
   end
 end
 
