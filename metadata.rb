96d41c87d1988157662db32712d71411d8629456
akahige <akahigeg@gmail.com>
2013-10-16 22:47:48 +0900

    edit metadata

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,13 @@ name             "rbenv-install-rubies"
 maintainer       "akahigeg"
 maintainer_email "akahigeg@gmail.com"
 license          "MIT"
-description      "Install ruby by rbenv"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+description      "Install ruby by using rbenv"
 version          "0.2"
 depends          "rbenv"
+
+recipe "rbenv-install-rubies::default", "Install rubyies that specified in node attributes."
+recipe "rbenv-install-rubies::keep-group-write", "To keep group write of rbenv versions directory for installing gems by rbenv group user."
+
+%w{ centos redhat fedora ubuntu debian amazon oracle}.each do |os|
+  supports os
+end
